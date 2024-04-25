180c95ca36234e05add70f84b01ecf9f87fd0ee7
gui777x <guizin.clementino@gmail.com>
2024-04-25 12:28:38 -0300

    Criação da classe Genre

---
+++ GalloFlix/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GAlloFlix.Models;
+
+public class ErrorViewModel
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ GalloFlix/Models/Genre.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GalloFlix.Models;
+
+[Table("Genre")]
+public class Genre
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public byte id { get; set; }
+
+    [Display(Name = "Nome")]
+    public string Name { get; set; }
+
+
+}
