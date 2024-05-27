f6ad8e55164faaa91ebb837760a755253d624c0c
gui777x <guizin.clementino@gmail.com>
2024-05-27 11:08:24 -0300

    Configuração do serviço do Identity, e codificação do método para popular usuário

--- GalloFlix/Data/AppDbContext.cs
+++ GalloFlix/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 using GalloFlix.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace GalloFlix.Data;
@@ -9,6 +10,7 @@ public class AppDbContext : DbContext
         {
         }
 
+        public DbSet<AppUser> AppUsers { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<MovieGenre> movieGenres { get; set; }
@@ -32,5 +34,109 @@ public class AppDbContext : DbContext
                 .HasForeignKey(mg => mg.GenreId);
         #endregion
 
+        #region Popular os dados de Usuario
+        // Perfil - IdentityRole
+        List<IdentityRole> roles = new()
+        {
+                new IdentityRole()
+                {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Administrador",
+                        NormalizedName = "ADMINISTRADOR"
+                },
+                new IdentityRole()
+                {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Moderador",
+                        NormalizedName = "MODERADOR"
+                },
+                new IdentityRole()
+                {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Usuário",
+                        NormalizedName = "USUÁRIO"
+                }
+
+        };
+        builder.Entity<IdentityRole>().HasData(roles);
+
+        // Conta de Usuário - IdentityUser
+        List<IdentityUser> users = new()
+        {
+                new IdentityUser()
+                {
+                        Id = Guid.NewGuid().ToString(),
+                        Email = "admin@galloflix.com",
+                        NormalizedEmail = "ADMIN@GALLOFLIX.COM",
+                        UserName = "Admin",
+                        NormalizedUserName = "ADMIN",
+                        EmailConfirmed = true,
+                        LockoutEnabled = false,
+                },
+                new IdentityUser()
+                {
+                        Id = Guid.NewGuid().ToString(),
+                        Email = "user@gmail.com",
+                        NormalizedEmail = "USER@GMAIL.COM",
+                        UserName = "User",
+                        NormalizedUserName = "USER",
+                        EmailConfirmed = true,
+                        LockoutEnabled = true,
+                }
+        };
+        foreach (var user in users)
+        {
+                PasswordHasher<IdentityUser> pass = new();
+                pass.HashPassword(user, "@Etec123");
+        }
+
+        builder.Entity<IdentityUser>().HasData(users);
+
+        // DAdos pessoais do Usuário - AppUser
+        List<AppUser> appUsers = new()
+        {
+                new AppUser()
+                {
+                        AppUserId = users[0].Id,
+                        Name = "Guilherme Alexandre Clementino",
+                        Birthday = DateTime.Parse("27/07/2007"),
+                        Photo = "/img/users/avatar.png",
+                },
+                new AppUser()
+                {
+                        AppUserId = users[1].Id,
+                        Name = "Robertinho",
+                        Birthday = DateTime.Parse("20/02/2000"),
+                }
+
+        };
+        builder.Entity<AppUser>().HasData(appUsers);
+
+        // Perfis dos Usuários - IdentityUserRole
+        List<IdentityUserRole<String>> userRoles = new()
+        {
+                new IdentityUserRole<string>()
+                {
+                        UserId = users[0].Id,
+                        RoleId = roles[0].Id
+                },
+                new IdentityUserRole<string>()
+                {
+                        UserId = users[0].Id,
+                        RoleId = roles[1].Id
+                },
+                new IdentityUserRole<string>()
+                {
+                        UserId = users[0].Id,
+                        RoleId = roles[2].Id
+                },
+                new IdentityUserRole<string>()
+                {
+                        UserId = users[1].Id,
+                        RoleId = roles[2].Id
+                }
+        };
+        builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
+        #endregion
     }
 }
